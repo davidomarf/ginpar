fedd6bd61f65a718ef56ee20b8b9d9fdd0e206be
David Omar Flores Chávez <david@davidomar.com>
2019-10-04 02:29:47 -0500

    Change the url of the libraries

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-p5_path = "../static/lib/p5.js"
-delaunator_path = "../static/lib/delaunator.min.js"
+p5_path = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"
+delaunator_path = "https://unpkg.com/delaunator@4.0.1/delaunator.min.js"
