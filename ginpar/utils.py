ad2cc540a6035b29a482524bff20717637fc07c5
David Omar Flores Chávez <david@davidomar.com>
2019-10-18 12:07:57 -0500

    Add `info()` to echo in standard fg color

--- ginpar/utils.py
+++ ginpar/utils.py
@@ -7,17 +7,23 @@
 
 import click
 
+
 def unkebab(s):
     return " ".join(s.split("-"))
 
 
 # ------------------------------ Throw messages ------------------------------ #
 
+def info(m):
+    click.secho(m)
+
 def success(m):
     click.secho(m, fg="green")
 
+
 def error(m):
     click.secho(m, fg="red")
+
 
 def alert(m):
     click.secho(m, fg="yellow")
