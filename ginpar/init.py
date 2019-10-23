933252042a2137cbc1f3147ee1b0a1ac793165f2
David Omar Flores Chávez <david@davidomar.com>
2019-10-23 17:43:13 -0500

    Write a config.yaml file when initializing a new projecy

--- ginpar/init.py
+++ ginpar/init.py
@@ -27,47 +27,55 @@ of the project to initialize::
 import os
 
 import click
-from jinja2 import Environment, FileSystemLoader
+import yaml
 
 from ginpar.utils.echo import info, echo, success, error, alert
 from ginpar.utils.files import create_file, create_folder, try_remove
 from ginpar.utils.strings import space_to_kebab
 
-_TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
-_jinja_env = Environment(loader=FileSystemLoader(_TEMPLATES_DIR), trim_blocks=True)
 
-
-def prompt_site_config():
+def prompt_site_config(quick):
     """Echo the prompts and create the configuration dict.
 
     Echo the instructions and configuration fields, store each input,
     and create a dictionary containing those values.
+
+    Parameters
+    ----------
+    quick : bool
+        Returns the default values immediatle if True.
 
     Returns
     -------
     dict
         Used to generate the site configuration file.
     """
+    site = {
+        "author": "David Omar",
+        "sitename": "My site",
+        "description": "This is a Ginpar project",
+        "url": "/",
+        "theme": "gart",
+        "content_path": "sketches",
+        "build_path": "public",
+    }
+
+    if quick:
+        return site
+
     info("Welcome to ginpar! We'll ask for some values to initialize your project.")
     echo("")
-    sitename = click.prompt("Site name", default="My Site")
-    description = click.prompt("Description", default="Cool site")
-    author = click.prompt("Author", default="John Doe")
-    url = click.prompt("url", default="johndoe.com")
+    site["sitename"] = click.prompt("Site name", default=site["sitename"])
+    site["description"] = click.prompt("Description", default=site["description"])
+    site["author"] = click.prompt("Author", default=site["author"])
+    site["url"] = click.prompt("url", default=site["url"])
     info("\nIf you're unsure about the next prompts, accept the defaults")
     echo("")
-    theme = click.prompt("Theme", default="gart")
-    content_path = click.prompt("Sketches path", default="sketches")
-    build_path = click.prompt("Build path", default="public")
-    return {
-        "author": author,
-        "sitename": sitename,
-        "description": description,
-        "url": url,
-        "theme": theme,
-        "content_path": content_path,
-        "build_path": build_path,
-    }
+    site["theme"] = click.prompt("Theme", default=site["theme"])
+    site["content_path"] = click.prompt("Sketches path", default=site["content_path"])
+    site["build_path"] = click.prompt("Build path", default=site["build_path"])
+
+    return site
 
 
 def init(force, quick):
@@ -81,36 +89,28 @@ def init(force, quick):
     quick : bool
         Skip prompts when true.
     """
-    _config_template = _jinja_env.get_template("config.json.jinja2")
 
     if force:
         alert("You're forcing the initialization.")
         alert("This will replace any existent file relevant to the project.")
         click.confirm("Do you want to proceed?", abort=True)
 
-    if quick:
-        content_path = os.path.join("my-site", "sketches")
-        config_json = os.path.join("my-site", "config.json")
-        config_dict = _config_template.render()
-    else:
-        site = prompt_site_config()
+    site = prompt_site_config(quick)
+    path = space_to_kebab(site["sitename"]).lower()
 
-        path = space_to_kebab(site["sitename"]).lower()
-        print(site["content_path"])
-        echo("\n---\n")
+    content_path = os.path.join(path, site["content_path"])
+    config_yaml = os.path.join(path, "config.yaml")
 
-        content_path = os.path.join(path, site["content_path"])
-        config_json = os.path.join(path, "config.json")
-        config_dict = _config_template.render(site)
-
+    echo("\n---\n")
+    
     if force:
         echo("\n---\n")
-        try_remove(content_path)
-        try_remove(config_json)
+        try_remove(path)
         echo("\n---\n")
 
     create_folder(content_path)
-    create_file(config_json, config_dict)
+    with open(config_yaml, "w") as file:
+        yaml.dump(site, file)
 
     echo("\n---\n")
     success(
