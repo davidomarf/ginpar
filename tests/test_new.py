bd17a01b2ea4a41b62979058247a46bf22399bf8
David Omar Flores Chávez <david@davidomar.com>
2019-10-23 19:26:48 -0500

    Add a test for the default behaviour in ginpar.new

--- tests/test_new.py
+++ tests/test_new.py
@@ -1,8 +1,25 @@
+import os
+
 from click.testing import CliRunner
 from ginpar import cli
 
 def test_new():
-  runner = CliRunner()
-  result = runner.invoke(cli, ["new", 'hey'])
-  assert result.exit_code == 0
-  assert "You're in new" in result.output
+    """New works properly on the default scenario.
+
+    Default scenario is when the directory of the new sketch doesn't
+    exist in the source_path..
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        ## Initialize a new project and cd it
+        runner.invoke(cli, ["init", "-q"])
+        os.chdir("my-site")
+
+        result = runner.invoke(cli, ["new", "test"])
+        
+        data_path = os.path.join("sketches", "test", "data.yaml")
+        sketch_path = os.path.join("sketches", "test", "sketch.js")
+        assert result.exit_code == 0
+        assert os.path.isfile(data_path)
+        assert os.path.isfile(sketch_path)
+       
