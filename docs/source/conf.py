11f5b029fbcce765357865328547644a411d21f7
David Omar Flores Chávez <david@davidomar.com>
2019-11-07 18:35:13 -0600

    Bump versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, David Omar Flores Chavez'
 author = 'David Omar Flores Chavez'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.8.3'
+release = 'v1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
